8780280f1f0a133667c2dbd751cf18ef48c943c4
saeel123 <saeel@dccper.com>
2017-11-21 18:02:04 +0530

    url test

--- index.js
+++ index.js
@@ -173,39 +173,34 @@ function processMessage(event) {
             //   }
             // }
 
-              // Pincode.findByPincode(formattedMsg, function (err, address) {
-              //
-							// 	if (err) {
-							// 		sendMessage(senderId, {text: "error occured while search"});
-							// 	} else if (address === null){
-              //
-              //     //save the requested pincode in db
-							// 		let newPincode = new Pincode({
-							// 			pincode: formattedMsg
-							// 		});
-              //
-							// 		Pincode.addPincode(newPincode, function (newPincode, err) {
-							// 			if (!err) {
-							// 				sendMessage(senderId, {text: "some error occured while proccesing your request"});
-							// 			} else {
-							// 				sendMessage(senderId, {text: "we dint find that pin in our db. we proccssed your request"});
-							// 			}
-							// 		});
-							// 	} else {
-              //     //requested pincode is already there but no address
-							// 		//sendMessage(senderId, {text: question['question']});
-							// 		if (!address['address']) {
-							// 			sendMessage(senderId, {text: "we dont have address for this pin in our db"});
-							// 		} else {
-							// 			sendMessage(senderId, {text: address['address']});
-							// 		}
-							// 	}
-							// });
+              Pincode.findByPincode(formattedMsg, function (err, address) {
 
+								if (err) {
+									sendMessage(senderId, {text: "error occured while search"});
+								} else if (address === null){
 
-              // if (true) {
-              //
-              // }
+                  //save the requested pincode in db
+									let newPincode = new Pincode({
+										pincode: formattedMsg
+									});
+
+									Pincode.addPincode(newPincode, function (newPincode, err) {
+										if (!err) {
+											sendMessage(senderId, {text: "some error occured while proccesing your request"});
+										} else {
+											sendMessage(senderId, {text: "we dint find that pin in our db. we proccssed your request"});
+										}
+									});
+								} else {
+                  //requested pincode is already there but no address
+									//sendMessage(senderId, {text: question['question']});
+									if (!address['address']) {
+										sendMessage(senderId, {text: "we dont have address for this pin in our db"});
+									} else {
+										sendMessage(senderId, {text: address['address']});
+									}
+								}
+							});
 
 						}
         } else if (message.attachments) {
