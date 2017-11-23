35ccbab4acf55ef53c5b3231566bd70c5321b92a
saeel123 <saeel@dccper.com>
2017-11-23 11:38:02 +0530

    persistant menu

--- index.js
+++ index.js
@@ -235,23 +235,23 @@ function getPincodeAddress(userId, pincode) {
 
 
 function addPersistentMenu(){
- request({
-    url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json:{
-  "get_started":{
-    "payload":"GET_STARTED_PAYLOAD"
-   }
- }
-}, function(error, response, body) {
-    console.log("Add persistent menu " + response)
-    if (error) {
-        console.log('Error sending messages: ', error)
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error)
-    }
-})
+//  request({
+//     url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+//     qs: { access_token: PAGE_ACCESS_TOKEN },
+//     method: 'POST',
+//     json:{
+//   "get_started":{
+//     "payload":"GET_STARTED_PAYLOAD"
+//    }
+//  }
+// }, function(error, response, body) {
+//     console.log("Add persistent menu " + response)
+//     if (error) {
+//         console.log('Error sending messages: ', error)
+//     } else if (response.body.error) {
+//         console.log('Error: ', response.body.error)
+//     }
+// })
  request({
     url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
     qs: { access_token: PAGE_ACCESS_TOKEN },
