ff9f5dad0ee7c53433d0d64c29410f03676845da
saeel123 <saeel@dccper.com>
2017-11-23 17:45:40 +0530

    quick

--- index.js
+++ index.js
@@ -159,9 +159,292 @@ function processMessage(event) {
                                       }
                                   ]
                                 };
+                    let reply3 = {
+                              text: "Do people create personal profiles?",
+                              quick_replies: [
+                                  {
+                                      "content_type": "text",
+                                      "title": "Yes",
+                                      "payload": "login"
+                                  },
+                                  {
+                                      "content_type": "text",
+                                      "title": "No",
+                                      "payload": "login"
+                                  }
+                              ]
+                            };
+                    let reply4 = {
+                                  text: "How will you make money from your app?",
+                                  quick_replies: [
+                                      {
+                                          "content_type": "text",
+                                          "title": "Upfront Cost",
+                                          "payload": "login"
+                                      },
+                                      {
+                                          "content_type": "text",
+                                          "title": "In-App Purchase",
+                                          "payload": "login"
+                                      },
+                                      {
+                                          "content_type": "text",
+                                          "title": "Free",
+                                          "payload": "login"
+                                      }
+                                  ]
+                                };
+                      let reply5 = {
+                                    text: "Does your app need to connect with your website?",
+                                    quick_replies: [
+                                        {
+                                            "content_type": "text",
+                                            "title": "Yes",
+                                            "payload": "login"
+                                        },
+                                        {
+                                            "content_type": "text",
+                                            "title": "No",
+                                            "payload": "login"
+                                        }
+                                    ]
+                                  };
+                          let reply6 = {
+                          text: "Does your app need a Back-end",
+                          quick_replies: [
+                                                    {
+                                                        "content_type": "text",
+                                                        "title": "Yes",
+                                                        "payload": "login"
+                                                    },
+                                                    {
+                                                        "content_type": "text",
+                                                        "title": "No",
+                                                        "payload": "login"
+                                                    }
+                                                ]
+                                              };
+                          let reply7           = {
+                          text: "Do you need Server and Hosting",
+                          quick_replies: [
+                                                    {
+                                                        "content_type": "text",
+                                                        "title": "Yes",
+                                                        "payload": "login"
+                                                    },
+                                                    {
+                                                        "content_type": "text",
+                                                        "title": "No",
+                                                        "payload": "login"
+                                                    }
+                                                ]
+                                              };
+                      let reply8           = {
+                      text: "How nice should your app look?",
+                      quick_replies: [
+                                                {
+                                                    "content_type": "text",
+                                                    "title": "Bare-bones",
+                                                    "payload": "login"
+                                                },
+                                                {
+                                                    "content_type": "text",
+                                                    "title": "Stock",
+                                                    "payload": "login"
+                                                },
+                                                {
+                                                    "content_type": "text",
+                                                    "title": "Beautiful",
+                                                    "payload": "login"
+                                                }
+                                            ]
+                                          };
+            let reply9          = {
+            text: "Do you need an app icon?",
+            quick_replies: [
+                                      {
+                                          "content_type": "text",
+                                          "title": "Yes, I need an app icon",
+                                          "payload": "login"
+                                      },
+                                      {
+                                          "content_type": "text",
+                                          "title": "No, I don't need an app icon",
+                                          "payload": "login"
+                                      }
+                                  ]
+                                };
+            let reply10           = {
+            text: "Do you want your app to show advertisements?",
+            quick_replies: [
+                                      {
+                                          "content_type": "text",
+                                          "title": "Yes",
+                                          "payload": "login"
+                                      },
+                                      {
+                                          "content_type": "text",
+                                          "title": "No",
+                                          "payload": "login"
+                                      }
+                                  ]
+                                };
+              let reply11           = {
+              text: "What languages does this need to support?",
+              quick_replies: [
+                                        {
+                                            "content_type": "text",
+                                            "title": "English (US)",
+                                            "payload": "login"
+                                        },
+                                        {
+                                            "content_type": "text",
+                                            "title": "Indique Language",
+                                            "payload": "login"
+                                        },
+                                        {
+                                            "content_type": "text",
+                                            "title": "English (US) + Indique Language/Languages",
+                                            "payload": "login"
+                                        }
+                                    ]
+                                  };
+              let reply12           = {
+              text: "What devices do you want it to support?",
+              quick_replies: [
+                                        {
+                                            "content_type": "text",
+                                            "title": "Phone",
+                                            "payload": "login"
+                                        },
+                                        {
+                                            "content_type": "text",
+                                            "title": "Tablet",
+                                            "payload": "login"
+                                        },
+                                        {
+                                            "content_type": "text",
+                                            "title": "Both",
+                                            "payload": "login"
+                                        }
+                                    ]
+                                  };
+          let reply13           = {
+          text: "Should your app be displayed in Portrait or Landscape form?",
+          quick_replies: [
+                                    {
+                                        "content_type": "text",
+                                        "title": "Portrait",
+                                        "payload": "login"
+                                    },
+                                    {
+                                        "content_type": "text",
+                                        "title": "Landscape",
+                                        "payload": "login"
+                                    },
+                                    {
+                                        "content_type": "text",
+                                        "title": "Both",
+                                        "payload": "login"
+                                    }
+                                ]
+                              };
+                              let reply14           = {
+                              text: "Do you have an existing website? ",
+                              quick_replies: [
+                                                        {
+                                                            "content_type": "text",
+                                                            "title": "Yes",
+                                                            "payload": "login"
+                                                        },
+                                                        {
+                                                            "content_type": "text",
+                                                            "title": "No",
+                                                            "payload": "login"
+                                                        }
+                                                    ]
+                                                  };
+
+                  let reply15           = {
+                  text: "What stage is your Project in?",
+                  quick_replies: [
+                                          {
+                                              "content_type": "text",
+                                              "title": "Idea",
+                                              "payload": "login"
+                                          },
+                                          {
+                                              "content_type": "text",
+                                              "title": "Sketches",
+                                              "payload": "login"
+                                          },
+                                          {
+                                                "content_type": "text",
+                                                "title": "Development",
+                                                "payload": "login"
+                                            },
+                                            {
+                                                "content_type": "text",
+                                                "title": "Prototype",
+                                                "payload": "login"
+                                            }
+                                      ]
+                                    };
+
+              let reply16           = {
+              text: "What is your budget for the app?",
+              quick_replies: [
+                                      {
+                                          "content_type": "text",
+                                          "title": "Less than US$20000",
+                                          "payload": "login"
+                                      },
+                                      {
+                                          "content_type": "text",
+                                          "title": "US$20000 - US$50000",
+                                          "payload": "login"
+                                      },
+                                      {
+                                            "content_type": "text",
+                                            "title": "US$50000-US$100000",
+                                            "payload": "login"
+                                        },
+                                        {
+                                            "content_type": "text",
+                                            "title": "More than US$100000",
+                                            "payload": "login"
+                                        }
+                                  ]
+                                };
 
 
-        if (message.text) {
+
+                    let reply17           = {
+                    text: "What is the time frame for this app?",
+                    quick_replies: [
+                                            {
+                                                "content_type": "text",
+                                                "title": "Less than 1 Month",
+                                                "payload": "login"
+                                            },
+                                            {
+                                                "content_type": "text",
+                                                "title": "1-2 Months",
+                                                "payload": "login"
+                                            },
+                                            {
+                                                  "content_type": "text",
+                                                  "title": "2-3 Months",
+                                                  "payload": "login"
+                                              },
+                                              {
+                                                  "content_type": "text",
+                                                  "title": "More than 3 months",
+                                                  "payload": "login"
+                                              }
+                                        ]
+                                      };
+                  if (message.text) {
 						var formattedMsg = message.text.toLowerCase().trim();
 
 						if (formattedMsg === "hi"  ) {
@@ -172,12 +455,30 @@ function processMessage(event) {
               console.log("formattedMsg");
               console.log(formattedMsg);
 
+              return formattedMsg;
+
               switch (formattedMsg) {
                 case "android":
                 case "ios":
                 case "both":
                  sendMessage(senderId,reply2);
                   break;
+                case "Email":
+                case "Social":
+                case "Phone Number":
+                case "No, not required":
+                 sendMessage(senderId,reply3);
+                  break;
+                  case "Yes":
+                  case "No":
+                   sendMessage(senderId,reply4);
+                    break;
+                  case "Email":
+                  case "Social":
+                  case "Phone Number":
+                  case "No, not required":
+                   sendMessage(senderId,reply5);
+                    break;
                 default:
                 sendMessage(senderId, {text: "Will get back to you soon"});
               }
